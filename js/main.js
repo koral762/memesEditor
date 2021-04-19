de35df209ae83960e485ff54d8641d8ca503c947
koral <koral762@gmail.com>
2021-04-19 14:53:33 +0300

    photo gallery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,73 @@
 'use strict'
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 };
+
+var gImgs = [
+    {
+        id: 1,
+        url: 'img/1.jpg',
+        keywords: ['tramp', 'politics']
+    },
+    {
+        id: 2,
+        url: 'img/2.jpg',
+        keywords: ['puppy', 'cute']
+    },
+    {
+        id: 3,
+        url: 'img/3.jpg',
+        keywords: ['puppy', 'cute', 'baby', 'sleep']
+    },
+    {
+        id: 4,
+        url: 'img/4.jpg',
+        keywords: ['sleep', 'cat', 'keyboard', 'bored']
+    }
+];
+
+var gMeme = {
+    selectedImgId: 2,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
+
+var gCanvas;
+var gCtx;
+
+function oninit() {
+    gCanvas = document.getElementById('my-canvas')
+    gCtx = gCanvas.getContext('2d')
+}
+
+function onImageClicked(imgId) {
+
+    gMeme.selectedImgId = parseInt(imgId);
+    renderImgToCanvas(gMeme.selectedImgId);
+
+}
+
+
+function onDrawText() {
+
+    drawText(gMeme.lines[0].txt, gCanvas.width / 2, gCanvas.height / 8);
+}
+
+
+
+function onInputText(txt) {
+    gMeme.lines[0].txt = txt;
+    setTimeout(() => {
+        onDrawText()
+    }, 200);
+
+    var elMemeText = document.getElementById('meme-text');
+    elMemeText.value = '';
+
+}
--- js/memeService.js
+++ js/memeService.js
@@ -1,1 +1,33 @@
 'use strict'
+
+function renderImgToCanvas(imgId) {
+
+    var elImg = getImgById(imgId);
+    elImg = elImg.url;
+    
+    var img = new Image();
+    img.src = elImg;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    }
+
+}
+
+
+function drawText(text, x, y) {
+    console.log(text);
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'red'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '40px IMPACT'
+    gCtx.textAlign = 'center'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function getImgById(imgId) {
+    var imgObj = gImgs.find(function (img) {
+        return imgId === img.id
+    })
+    return imgObj;
+}
